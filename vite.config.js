763b2eaf6fa55f5aed779d9cb0b15f278ae4126b
MrForestino <borv2006@gmail.com>
2025-07-27 16:04:30 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  base: './', // дуже важливо для GitHub Pages
+  base: '/StudentsCardLocalStorage', // дуже важливо для GitHub Pages
   plugins: [handlebars()]
 });
